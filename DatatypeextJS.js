e0850af2676842ccfce393f054bc0059302ad975
ameenk007 <ameenk007@gmail.com>
2022-03-25 13:05:11 +0530

    7th commit/JS basics

--- DatatypeextJS.js
+++ DatatypeextJS.js
@@ -2,13 +2,17 @@
 function details()
  {
     var a = 20;
-    var n = Alex;
+    var n = "Peter Parker";
     var b = true;
     var h = 1.59;
     var D = null;
+    var income= "RS 1,00,000-2,00,000";
+    var k= ["HTML","CSS","JS"];
     document.write("Name: "+ n +"<br>");
     document.write("Age: "+ a +"<br>");
     document.write("Below 25: "+ b +"<br>");
     document.write("Height: "+ h +"<br>");
     document.write("Differently abled: "+ D +"<br>");
+    document.write("Income: " +income+"<br>");
+    document.write("Skills: " + k[0]+k[1]+ k[2] + "<br>");
  }
