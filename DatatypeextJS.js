f18a0e84fdd6ee52668e8f1f61c35652eae065a6
ameenk007 <ameenk007@gmail.com>
2022-03-25 11:46:02 +0530

    7th commit

---
+++ DatatypeextJS.js
@@ -0,0 +1,14 @@
+
+function details()
+ {
+    var a = 20;
+    var n = Alex;
+    var b = true;
+    var h = 1.59;
+    var D = null;
+    document.write("Name: "+ n +"<br>");
+    document.write("Age: "+ a +"<br>");
+    document.write("Below 25: "+ b +"<br>");
+    document.write("Height: "+ h +"<br>");
+    document.write("Differently abled: "+ D +"<br>");
+ }
